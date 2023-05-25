65fc21e30c47997155e814bde15e775bcd2f45fc
Diego Yanez <diegosyanez92@gmail.com>
2023-05-25 13:27:06 -0400

    redacted private info

---
+++ client.js
@@ -0,0 +1,13 @@
+import {createClient} from '@sanity/client'
+import {process} from 'dotenv'
+
+export const client = createClient({
+  projectId: process.env.React_APP_PROJECT_ID,
+  dataset: process.env.REACT_APP_DATASET,
+  useCdn: true,
+  apiVersion: "2023-05-01",
+});
+
+export async function getResumeList(){
+    const resumeList = await client.fetch('*[_type === resume]');
+}
