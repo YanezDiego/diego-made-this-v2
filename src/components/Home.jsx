df447be5f97e871c3db050481fd915b0867320c1
Diego Yanez <diegosyanez92@gmail.com>
2023-05-26 15:52:30 -0400

    displayed information using props

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,47 +1,55 @@
-import {useEffect ,useState } from "react";
+import { useEffect, useState } from "react";
 import { client } from "../../client";
-
+import ResumeList from "./ResumeListContainer";
 
 const Home = () => {
   const [jobList, setJobList] = useState(null);
-  
-  useEffect(() => {  
-    
-    client.fetch(`*[_type == "resume"]`)
-    .then((data) => setJobList(data))
-    .catch(console.error);
-    
-  },[])
 
+  useEffect(() => {
+    client
+      .fetch(`*[_type == "resume"] | order(startDate desc)`)
+      .then((data) => setJobList(data))
+      .catch(console.error);
+  }, []);
 
-  
-
-    return (
-      
-        <div className='flex items-start justify-center min-h-screen p-4 md:p-6 lg:p-16'>
-          <div className='container mx-auto'>
-            <div className='flex flex-col items-center text-center'>
-              <div className='title p-4'>
-                <h1 className='text-4xl md:text-6xl lg:text-8xl'>
-                  Hello! <br /> My name is Diego
-                </h1>
-              </div>
-              <div className='intro font-light text-gray-600 text-lg md:text-xl lg:text-2xl'>
-                <p>
-                  I am a{" "}
-                  <span className='underline'>
-                    {" "}
-                    highly experienced Full Stack Software Engineer
-                  </span>{" "}
-                  based in Brooklyn, NY.
-                </p>
-              </div>
+  return (
+    <>
+      <div className='flex items-start justify-center min-h-full md:min-h-[66vh] p-4 md:p-6 lg:p-16'>
+        <div className='container mx-auto'>
+          <div className='flex flex-col items-center text-center'>
+            <div className='title p-4'>
+              <h1 className='text-4xl md:text-6xl lg:text-8xl'>
+                Hello! <br /> My name is Diego
+              </h1>
+            </div>
+            <div className='intro font-light text-gray-600 text-lg md:text-xl lg:text-2xl'>
+              <p>
+                I am an{" "}
+                <span className='underline'>
+                  {" "}
+                  experienced Full Stack Software Engineer
+                </span>{" "}
+                based in Brooklyn, NY.
+              </p>
             </div>
           </div>
         </div>
-      
-    );
+      </div>
+      {/* <div className='flex flex-col md:flex-row lg:flex-row items-center  p-4 md:p-6 lg:p-16 '> */}
+      <div className='container mx-auto text-lg md:text-xl lg:text-2xl'>
+        <h2 className='text-center'>Past Experiences</h2>
+        <div className=''>
+          <div className='flex flex-col md:flex-row lg:flex-row items-center gap-3 p-4 md:p-6 lg:p-16'>
+            {jobList &&
+              jobList.map((job, index) => (
+                <ResumeList jobInfo={job} key={index} />
+              ))}
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
+    </>
+  );
+};
 
-}
-
-export default Home
+export default Home;
--- src/components/resumeListContainer.jsx
+++ src/components/resumeListContainer.jsx
@@ -1,8 +1,27 @@
-import { useEffect, useState } from "react";
+const ResumeList = (props) => {
+  const {
+    employer,
+    isCurrent,
+    startDate,
+    endDate,
+    position,
+    companyDescription,
+  } = props.jobInfo;
 
-const ResumeList = () => {
-    const [jobs, setJobs] = useState([]);
+  let workStart = new Date(startDate).getFullYear();
+  let workEnd = new Date(endDate).getFullYear();
 
+  return (
+    <div tabIndex='0'
+     className='bg-white rounded-lg shadow-sm p-4 md:p-6 hover:shadow-lg md:transform md:hover:-translate-y-1 md:hover:scale-105 md:transition md:duration-300'>
+
+        <h3>{employer}</h3>
+        <article>{companyDescription}</article>
+        <p>{position}</p>
+        <p>{isCurrent ? "Current" : `From ${workStart} to ${workEnd}`}</p>
+
+    </div>
+  );
 };
 
 export default ResumeList;
