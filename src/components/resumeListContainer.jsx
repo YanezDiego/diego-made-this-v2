fc7daa78f7018e3a6fc781737393f0027fec5420
Diego Yanez <diegosyanez92@gmail.com>
2023-05-31 14:44:38 -0400

    updated tailwindcss

--- src/components/resumeListContainer.jsx
+++ src/components/resumeListContainer.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/prop-types */
+
+
 const ResumeList = (props) => {
   const {
     employer,
@@ -6,22 +9,42 @@ const ResumeList = (props) => {
     endDate,
     position,
     companyDescription,
+    achievements,
   } = props.jobInfo;
 
   let workStart = new Date(startDate).getFullYear();
   let workEnd = new Date(endDate).getFullYear();
-
+debugger
   return (
-    <div tabIndex='0'
-     className='bg-white rounded-lg shadow-sm p-4 md:p-6 hover:shadow-lg md:transform md:hover:-translate-y-1 md:hover:scale-105 md:transition md:duration-300'>
+    <div
+      tabIndex='0'
+      className='bg-white rounded-lg shadow-sm p-4 hover:shadow-lg md:transform md:hover:-translate-y-1 md:hover:scale-105 md:transition md:duration-300'
+    >
+      <h3 className='bold text-lg md:text-xl lg:text-2xl'>{employer}</h3>
+      <div className='font-light pt-1'>
+        <p className='pt-1 text-stone-400 text-base '>
+          <span className='italic'>{position.trim()}</span>
+        </p>
+        <p className='py-1 text-base text-stone-400'>
+          <span className='italic'>
+            {isCurrent ? "Current" : `From ${workStart} to ${workEnd}`}
+          </span>
+        </p>
+        <p className='text-base'>{companyDescription}</p>
 
-        <h3>{employer}</h3>
-        <article>{companyDescription}</article>
-        <p>{position}</p>
-        <p>{isCurrent ? "Current" : `From ${workStart} to ${workEnd}`}</p>
-
+      </div>
     </div>
   );
 };
+
+
+ResumeList.defaultProps = {
+  employer: '',
+  isCurrent: false,
+  startDate: '',
+  endDate: '',
+  position: '',
+  companyDescription: '',
+};  
 
 export default ResumeList;
