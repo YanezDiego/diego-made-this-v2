81f712a692f3afbd405b61ac4763022fa95bf82a
Diego Yanez <diegosyanez92@gmail.com>
2023-05-31 15:53:50 -0400

    fixed spacing for flexrow card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -50,10 +50,10 @@ const Home = () => {
         </div>
       </div>
 
-      <div className=' mx-auto text-xl md:text-6xl lg:text-6xl'>
+      <div className='flex-wrap mx-auto text-xl md:text-6xl lg:text-6xl'>
         <h2 className='text-center font-medium'>Past Experiences</h2>
 
-        <div className='flex flex-col md:flex-row lg:flex-row  gap-3 p-4 lg:p-8'>
+        <div className='flex flex-col basis-1/4 p-4 md:flex-row lg:flex-row lg:p-8'>
           {jobList &&
             jobList.map((job, index) => (
               <ResumeList jobInfo={job} key={index} />
