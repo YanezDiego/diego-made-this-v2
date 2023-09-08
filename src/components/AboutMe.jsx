4fd71e248515ee728fe6f61097720d177992da23
Diego Yanez <diegosyanez92@gmail.com>
2023-09-08 14:32:19 -0400

    add about me information

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,32 @@
 const Contact = () => {
-    return (
-      <>
-        <div>This is the about me</div>
-      </>
-    );
-}
+  return (
+    <>
+      <div>
+        With three years of experience as a freelance full-stack software
+        engineer, I have gained expertise in various languages and frameworks
+        such as JavaScript, SQL, Ruby, TypeScript, HTML/HTML5, CSS/SCSS, React,
+        Gatsby, Ruby on Rails, GraphQL, and LINQ. Throughout my career, I have
+        had the opportunity to work on diverse projects, collaborating with
+        cross-functional teams to create user-friendly websites and web
+        applications. I am particularly passionate about accessibility and have
+        utilized tools like screen readers, keyboard navigation, and color
+        contrast checkers to test and improve accessibility in my work. In my
+        previous roles, I have been responsible for tasks such as gathering
+        customer data, automating reporting tools, and developing back-end APIs
+        using technologies like Node.js and MongoDB. I have also led the
+        development of front-end features, utilizing frameworks like React and
+        GraphQL to enhance user experiences. Additionally, I have had the
+        privilege of working as a technical coach, where I provided support to
+        students at all levels of their curriculum. This experience allowed me
+        to further refine my skills and contribute to the development of a
+        comprehensive online curriculum. I am enthusiastic about creating
+        innovative solutions and delivering exceptional results. If you are
+        looking for a friendly and skilled software engineer to contribute to
+        your project, I would love to connect and discuss how I can be of
+        assistance.
+      </div>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
