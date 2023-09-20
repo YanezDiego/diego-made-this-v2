04e6b4002ebfdcd7b73529a6b2410a8a18fa3eca
Diego Yanez <diegosyanez92@gmail.com>
2023-09-19 22:01:51 -0400

    add gap between job list

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
       <div className='flex-wrap mx-auto text-xl md:text-6xl lg:text-6xl'>
         <h2 className='text-center font-medium'>Past Experiences</h2>
 
-        <div className='flex flex-col basis-1/4 p-4 md:flex-row lg:flex-row lg:p-8'>
+        <div className='flex flex-col basis-1/4 p-4 md:flex-row md:gap-8 lg:flex-row lg:p-8'>
           {jobList &&
             jobList.map((job, index) => (
               <ResumeList jobInfo={job} key={index} />
