3ec5dcb463a21e833d24d4d17d576ccc974a3e00
Diego Yanez <diegosyanez92@gmail.com>
2024-08-26 10:59:31 -0400

    added project prop

--- src/components/Projects/ProjectListContainer.jsx
+++ src/components/Projects/ProjectListContainer.jsx
@@ -7,7 +7,9 @@ const ProjectListContainer = () => {
         <div className='flex-wrap mx-auto text-xl md:text-6xl lg:text-6xl'>
           <h2 className='text-center font-medium'>Projects</h2>
           <div className='flex flex-col basis-1/4 p-4 md:flex-row md:gap-8 lg:flex-row lg:p-8'>
-             <ProjectList/>
+             {projects && projects.map((project) => (
+              <ProjectList key={project.id} project={project} />
+             ))}
           </div>
         </div>
       </>
